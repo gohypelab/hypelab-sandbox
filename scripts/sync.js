749ef212cf57578b87b0633e61259e136e359daf
Ed Weng <edweng@gmail.com>
2023-02-24 00:42:10 -0800

    Adding more ignore files to the sync / test scripts.

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,51 +1,51 @@
-
 const { promises } = require('fs');
 const { exec } = require('./utils/exec');
 
 const fetchPath = async () => {
-    try {
-        const path = await promises.readFile('.path', 'utf8');
-        return path;
-    } catch (e) {
-        const update = await ask('Enter your local hypelab-ts path:');
-        await promises.writeFile('.path', update, 'utf8');
-        return update;
-    }
+  try {
+    const path = await promises.readFile('.path', 'utf8');
+    return path;
+  } catch (e) {
+    const update = await ask('Enter your local hypelab-ts path:');
+    await promises.writeFile('.path', update, 'utf8');
+    return update;
+  }
 };
 
 const run = async () => {
-    const path = await fetchPath();
+  const path = await fetchPath();
 
-    const providers = await promises.readdir('.');
-    const ignore = ['scripts', 'package.json'];
+  const providers = await promises.readdir('.');
 
-    for (const provider of providers) {
-        if (ignore.includes(provider) || provider.startsWith('.')) continue;
+  const ignore = ['scripts', 'package.json', 'package-lock.json', 'README.md', 'scripts'];
 
-        const providerPath = `${path}/dist/providers/${provider}`;
-        const built = await alreadyExists(providerPath);
+  for (const provider of providers) {
+    if (ignore.includes(provider) || provider.startsWith('.')) continue;
 
-        if (!built) {
-            console.error('You need to build hypelab-ts before you can sync');
-            return;
-        }
+    const providerPath = `${path}/dist/providers/${provider}`;
+    const built = await alreadyExists(providerPath);
 
-        const package = await json(providerPath + '/package.json');
-        const projects = await promises.readdir(provider);
-
-        for (const project of projects) {
-            if (ignore.includes(project) || project.startsWith('.')) continue;
-            
-            try {
-                const modulePath = `${provider}/${project}/node_modules/${package.name}`;
-                await exec('mkdir', ['-p', modulePath]);
-                await exec('rsync', ['-avz', providerPath + '/', modulePath]);
-            } catch (e) {
-                console.error(`Failed to sync module from ${path} to ${provider}`);
-            }
-        }
+    if (!built) {
+      console.error('You need to build hypelab-ts before you can sync');
+      return;
     }
-}
+
+    const package = await json(providerPath + '/package.json');
+    const projects = await promises.readdir(provider);
+
+    for (const project of projects) {
+      if (ignore.includes(project) || project.startsWith('.')) continue;
+
+      try {
+        const modulePath = `${provider}/${project}/node_modules/${package.name}`;
+        await exec('mkdir', ['-p', modulePath]);
+        await exec('rsync', ['-avz', providerPath + '/', modulePath]);
+      } catch (e) {
+        console.error(`Failed to sync module from ${path} to ${provider}`);
+      }
+    }
+  }
+};
 
 run();
 
@@ -53,32 +53,33 @@ run();
 
 const readline = require('readline');
 
-const ask = prompt => new Promise(resolve => {
+const ask = (prompt) =>
+  new Promise((resolve) => {
     const interface = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
+      input: process.stdin,
+      output: process.stdout,
     });
 
-    interface.question(prompt, result => {
-        resolve(result);
-        interface.close();
+    interface.question(prompt, (result) => {
+      resolve(result);
+      interface.close();
     });
-});
+  });
 
-const json = async path => {
-    try {
-        const content = await promises.readFile(path, 'utf8');
-        return JSON.parse(content);
-    } catch (e) {
-        return null;
-    }
+const json = async (path) => {
+  try {
+    const content = await promises.readFile(path, 'utf8');
+    return JSON.parse(content);
+  } catch (e) {
+    return null;
+  }
 };
 
-const alreadyExists = async path => {
-    try {
-        await promises.stat(path);
-        return true;
-    } catch (e) {
-        return false;
-    }
+const alreadyExists = async (path) => {
+  try {
+    await promises.stat(path);
+    return true;
+  } catch (e) {
+    return false;
+  }
 };
--- scripts/test.js
+++ scripts/test.js
@@ -1,28 +1,27 @@
-
 const { promises } = require('fs');
 const { exec } = require('./utils/exec');
 
 const run = async () => {
-    const providers = await promises.readdir('.');
-    const ignore = ['scripts', 'package.json'];
+  const providers = await promises.readdir('.');
+  const ignore = ['scripts', 'package.json', 'package-lock.json', 'README.md', 'scripts'];
 
-    for (const provider of providers) {
-        if (ignore.includes(provider) || provider.startsWith('.')) continue;
+  for (const provider of providers) {
+    if (ignore.includes(provider) || provider.startsWith('.')) continue;
 
-        const projects = await promises.readdir(provider);
+    const projects = await promises.readdir(provider);
 
-        for (const project of projects) {
-            if (ignore.includes(project) || project.startsWith('.')) continue;
+    for (const project of projects) {
+      if (ignore.includes(project) || project.startsWith('.')) continue;
 
-            try {
-                const result = await exec('npm', ['test'], { cwd: provider + '/' + project });
-                console.log(result);
-            } catch (e) {
-                console.error(`Failed to run tests for ${provider}/${project}`);
-                console.error(e);
-            }
-        }
+      try {
+        const result = await exec('npx', ['cypress', 'run', '--component'], { cwd: provider + '/' + project });
+        console.log(result);
+      } catch (e) {
+        console.error(`Failed to run tests for ${provider}/${project}`);
+        console.error(e);
+      }
     }
+  }
 };
 
 run();
